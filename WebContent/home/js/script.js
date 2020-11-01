e2fd30c621cfbf56b4d96201a60171b598fd77e5
Paulo Guedes <pauloguedes@m2group.co>
2020-11-01 20:16:39 -0300

    Atualizando repositório

---
+++ WebContent/home/js/script.js
@@ -0,0 +1,27 @@
+
+$(function () {
+  $('#recompensa1').popover({
+    trigger: 'hover',
+    placement: 'right'
+  });
+  $('#recompensa2').popover({
+    trigger: 'hover',
+    placement: 'right'
+  });
+  $('#recompensa3').popover({
+    trigger: 'hover',
+    placement: 'right'
+  });
+  $('#recompensa4').popover({
+    trigger: 'hover',
+    placement: 'right'
+  });
+  $('#recompensa5').popover({
+    trigger: 'hover',
+    placement: 'right'
+  });
+  $('.chat-button').popover({
+    trigger: 'hover',
+    placement: 'top'
+  });
+})
